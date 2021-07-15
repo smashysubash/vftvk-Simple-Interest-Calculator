fc190de11fcba608313353fd66c6d2337e0180d3
Subash S <76615324+smashysubash@users.noreply.github.com>
2021-07-15 12:34:54 +0530

    a

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function compute()
 {
     p = document.getElementById("principal").value;
 }
-        
+function rangeSlide(value) {
+    document.getElementById('rangeValue').innerHTML = value;
+}        
