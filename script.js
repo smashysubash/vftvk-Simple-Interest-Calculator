0cedd2e765688fa027e8f1944a6c2076f9486f47
Subash S <76615324+smashysubash@users.noreply.github.com>
2021-08-02 21:34:37 +0530

    new js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let p = document.getElementById("principal").value;
+    let r = document.getElementById("rate").value;
+    let y = document.getElementById("years").value;
+    let i = document.getElementById("Interest").value;
+    if(check(p,r,y)){
+
+    }
 }
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
-}        
+}  
+function check(p,r,y){
+    if(p==""||r==""||y==""){
+        alert("Value must not be empty \n Enter the value");
+        return false;
+    }else{
+        return true;
+    }
+}      
