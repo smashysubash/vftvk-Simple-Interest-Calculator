fb940ed50858c59536940b0583bdcbdf23c1441b
Subash S <76615324+smashysubash@users.noreply.github.com>
2021-08-03 12:19:10 +0530

    final

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function compute()
 {
-    let p = document.getElementById("principal").value;
-    let r = document.getElementById("rate").value;
-    let y = document.getElementById("years").value;
-    let i = document.getElementById("Interest").value;
-    if(check(p,r,y)){
-
+    var p = document.getElementById("principal").value;
+    var y = document.getElementById("years").value;
+    var i = document.getElementById("Interest").value;
+    var year = new Date().getFullYear() + parseInt(y);
+    if(check(p,y)){
+        var amount=(p*y*i)/100;
+        document.getElementById("result").innerHTML="If you deposit <mark>  "+p+"</mark> <br> at an interest rate of <mark>  "+i+"</mark> <br> you will recieve an amount of<mark>  "+amount+"</mark> <br> in the year<mark>  "+year+"</mark";
     }
 }
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }  
-function check(p,r,y){
-    if(p==""||r==""||y==""){
-        alert("Value must not be empty \n Enter the value");
+function check(p,y){
+    if(p==""||y==""){
+        alert("Value must not be empty \nEnter the value");
         return false;
+    }else if(p<0||y<0){
+        alert("Value must not be negative \nEnter positive value");
     }else{
         return true;
     }
